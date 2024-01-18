c55867f57024ade3bcef0aced8ec1aa7a427bfea
AugustoM94 <email@github.com>
2024-01-18 12:32:01 +0100

    fix

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.app')
 @section('content')
     <section class="container my-4">
-        <h1 class="text-danger">{{ $project->title }}</h1>
+        <h1 class="text-danger mb-3">{{ $project->title }}</h1>
         <div class="card w-50 bg-dark text-white border-white">
-            <img src="{{asset('storage/' . $project->img) }}" class="card-img-top" alt="{{ $project->title }}">
             <div class="card-body">
-                <h5 class="card-title">{{ $project->title }}</h5>
+                <h5 class="card-title text-primary ">Title: {{ $project->title }}</h5>
                 <p class="card-text">{{ $project->body }}</p>
-                <p>{{$project->category ? $project->category->name : 'No category'}}</p>
+                <p class="text-danger">Category: {{$project->category ? $project->category->name : 'No category' }}</p>
 
                 @forelse($project->technologies as $technology)
-                    <p>{{ $technology->name }} <i class="{{ $technology->icon }}"></i></p>
+                    <p>Tecnology: {{ $technology->name }} <i class="{{ $technology->icon }}"></i></p>
                     @empty
                     <p>No technologies</p>
                 @endforelse
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section class="container">
         @auth
-            <h1>Benvenuto admin {{ Auth::user()->name }}</h1>
+            <h1>Benvenuto  {{ Auth::user()->name }}</h1>
         @else
             <h1>Bentornato</h1>
         @endauth
